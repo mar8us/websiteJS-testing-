23ddbbd3feb6d2dcd437ea89fddaa7acd8d8740a
mar8us <120562521+mar8us@users.noreply.github.com>
2025-02-02 17:22:52 +0100

    Update card-input-display.js

--- js/card-input-display.js
+++ js/card-input-display.js
@@ -17,13 +17,26 @@ cardNumber.addEventListener("input", () =>
 
 const cardHolderDisplay = document.getElementById('card-holder-display');
 const cardHolderInput = document.getElementById('card-holder-input');
+const errorMessage = document.getElementById('error-message');
 
 cardHolderInput.addEventListener('input', function() {
     cardHolderDisplay.innerText = cardHolderInput.value;
+    const regex = /^[A-Za-z\s]*$/;
+    if(!regex.test(cardHolderDisplay.innerText))
+        errorMessage.style.display = 'block'; 
+    else
+        errorMessage.style.display = 'none';    
 });
 
 const expiryInput = document.getElementById('expiryDate');
 const validityDisplay = document.getElementById('validity');
+
+expiryInput.addEventListener('focus', function() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    expiryInput.min = `${yyyy}-${mm}`;
+});
 
 expiryInput.addEventListener('input', function() {
     const inputValue = expiryInput.value;
@@ -42,9 +55,16 @@ cvvInput.addEventListener("blur", () => {
     card.style.transform = "rotateY(0deg)"; 
 });
 
-const cardCvvDisplay = document.getElementById('cvv-display');
+const cardCvvDisplay = document.getElementById('cvv-display'); 
 const cardCvvInput = document.getElementById('cardCvv');
+const errorMessageCvv = document.getElementById('error-messageCvv');
 
 cardCvvInput.addEventListener('input', function() {
-    cardCvvDisplay.innerText = cardCvvInput.value;    
+    const regex = /^[0-9]*$/;  
+    if (!regex.test(cardCvvInput.value)) {  
+        errorMessageCvv.style.display = 'block';
+    } else {
+        errorMessageCvv.style.display = 'none';
+    }
+    cardCvvDisplay.innerText = cardCvvInput.value.replace(/\D/g, "");
 });
